673ff5f5705eeebd841737c87ffa729e6461bc69
Rodolfo Lorca <r.e.lorca@gmail.com>
2021-05-16 18:48:46 -0400

    agrgando la primera prueba

---
+++ Viajes Chile/assets/js/main.js
@@ -0,0 +1,7 @@
+$(function() {
+
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+}); 
+/* función javascript de tooltip que solo se ejecutará cuando el ratón pase por encima de lo requerido */
+/* función javascript de popover que agrega contenido dinámico a la página web */ 
