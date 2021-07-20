d448080f5fea283986251ade9ad11d05776b15bb
Rodolfo Lorca <r.e.lorca@gmail.com>
2021-07-20 13:40:11 -0400

    Subiendo Desafío PokeGuía final

---
+++ Desafío - PokeGuía/src/components/Card.vue
@@ -0,0 +1,43 @@
+<template> 
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card">
+          <img :src="url" class="card-img-top" alt="Pokemon" />
+          <div class="card-body">
+            <h1 class="card-title text-capitalize text-center"><b>{{ nombre }}</b></h1>
+            <h6><b><u>Movimientos</u></b></h6>
+            <ul>
+                <li v-for="(item, indice) in movimientos" :key="'movimiento_'+indice">{{ item.move.name }}</li>
+            </ul>
+            <h6><b><u>Habilidades</u></b></h6>
+            <ul>
+                <li v-for="(item, indice) in habilidades" :key="'habilidad_'+indice">{{ item.ability.name }}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      default: "",
+    },
+    habilidades: {
+      type: Array,
+      default: () => [],
+    },
+    movimientos: {
+      type: Array,
+      default: () => [],
+    },
+    nombre: String,
+  },
+};
+</script>
+<style scoped>
+</style>
