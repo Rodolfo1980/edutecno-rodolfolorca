4944accf4844fb9e20669cc249e444af64edc54f
Rodolfo Lorca <r.e.lorca@gmail.com>
2021-07-28 16:46:05 -0400

    Desafío Opcional Jest

---
+++ Desafío Opcional - Jest/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Header titulo="Bienvenidos al Desafío Opcional Jest" color="green"/>
+    <HelloWorld msg="Rodolfo Lorca - Desafío Latam 2021"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
