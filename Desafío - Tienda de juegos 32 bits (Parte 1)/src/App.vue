96fa4df329628d2fee8f8889bca003f1f30118c9
Rodolfo Lorca <r.e.lorca@gmail.com>
2021-07-29 14:38:29 -0400

    Desafío Tienda de Juegos 32 bits (Parte 1)

---
+++ Desafío - Tienda de juegos 32 bits (Parte 1)/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <h3>Rodolfo Lorca - Desafío Latam 2021</h3>
+      <img alt="logo" src="./assets/games.png">
+    </div>
+    <nav>
+        <router-link :to="{name: 'inicio'}">Inicio</router-link>
+        <router-link :to="{name: 'busquedas'}">Búsquedas</router-link>
+        <router-link :to="{name: 'ventas'}">Ventas</router-link>
+        <router-link :to="{name: 'total'}">Total</router-link>
+    </nav>
+    <transition name="vista">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+body{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+}
+.vista-enter-active, .vista-leave-active {
+    transition: opacity .1s;
+}
+.vista-enter, .vista-leave-to{
+    opacity: 0;
+}
+.header{
+  text-align: center;
+}
+h1, h2{
+  text-align:center;
+}
+nav{
+  width: 100%;
+  height:75px;
+  display:flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: #474445;
+}
+nav a{
+  color: #FFFFFF;
+  text-decoration: none;
+}
+a:hover{
+  color: #D2CCCD;
+}
+</style>
